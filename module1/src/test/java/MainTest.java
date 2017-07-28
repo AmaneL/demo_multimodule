1c9a36c098f678d8de0eeb289d92063c4db0dd4f
Amane Lambeau <amanedancez@gmail.com>
2017-07-28 15:28:09 +0200

    Create

---
+++ module1/src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest{
+
+    @Test
+    public void testUneMethode(){
+        assertEquals(5, 5);
+    }
+
+    @Test
+    public void testUneMethode2(){
+        assertFalse(5==6);
+    }
+}
